0d9e6568b806cff0d724d5d377bda1622a79ce32
Arunmaity2003 <arunmaity0610@gmail.com>
2026-05-08 17:01:34 +0530

    deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,34 +1,43 @@
-import express from "express"
-import "dotenv/config"
-import authRoutes from "./routes/auth.route.js"
-import userRoutes from "./routes/user.route.js"
-import chatRoutes from "./routes/chat.route.js"
-import connectDB from "./db/db.js"
-import cookieParser from "cookie-parser"
-import cors from "cors"
+import express from "express";
+import "dotenv/config";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import path from "path";
 
-const app = express()
+import authRoutes from "./routes/auth.route.js";
+import userRoutes from "./routes/user.route.js";
+import chatRoutes from "./routes/chat.route.js";
 
-connectDB() 
+import connectDB from "./db/db.js";
 
-app.get("/",(req,res) => {
-    res.send("you are in the right place")
-})
+const app = express();
+const PORT = process.env.PORT;
 
-app.use(express.json())
-app.use(cookieParser()); 
-app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true //allow frontend to send cookies
-}))
+const __dirname = path.resolve();
 
-app.use("/api/auth",authRoutes)
-app.use("/api/users",userRoutes)
-app.use("/api/chat",chatRoutes)
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true, // allow frontend to send cookies
+  })
+);
 
-const port = process.env.PORT || 5000
+app.use(express.json());
+app.use(cookieParser());
 
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/chat", chatRoutes);
 
-app.listen(port,() => {
-    console.log(`app in running on: http://localhost:${port}`)
-})
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  connectDB();
+});
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,8 @@
-import axios from "axios"
+import axios from "axios";
 
-//create an axios instance
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
+
 export const axiosInstance = axios.create({
-    baseURL:"http://localhost:5001/api",
-    withCredentials:true  //this means send cookies with the request
-})
+  baseURL: BASE_URL,
+  withCredentials: true, // send cookies with the request
+});
